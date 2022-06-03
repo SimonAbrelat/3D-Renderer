506f46ce5defdab6023593c8b3a6147c9231567a
Simon Abrelat <simon.abrelat@gmail.com>
2022-06-03 15:09:56 -0400

    Removed Triangle class, noticable speed improvement

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ imgy = int(imgx * aspect)
 # Utility Matrix Functions 
 #------------------------------------------------------------------
 def trans(x,y,z):
-    return np.matrix([
+    return np.array([
         [1,0,0,0],
         [0,1,0,0],
         [0,0,1,0],
@@ -21,7 +21,7 @@ def trans(x,y,z):
     ])
 
 def scale(x,y,z):
-    return np.matrix([
+    return np.array([
         [x,0,0,0],
         [0,y,0,0],
         [0,0,z,0],
@@ -29,17 +29,17 @@ def scale(x,y,z):
     ])
 
 def rotate(x,y,z):
-    return np.matrix([
+    return np.array([
         [ np.cos(x),np.sin(x),0,0],
         [-np.sin(x),np.cos(x),0,0],
         [0,0,1,0],
         [0,0,0,1]
-    ]) * np.matrix([
+    ]) * np.array([
         [np.cos(x),0,-np.sin(x),0],
         [0,1,0,0],
         [np.sin(x),0, np.cos(x),0],
         [0,0,0,1]
-    ]) * np.matrix([
+    ]) * np.array([
         [1,0,0,0],
         [0, np.cos(x),np.sin(x),0],
         [0,-np.sin(x),np.cos(x),0],
@@ -49,18 +49,17 @@ def rotate(x,y,z):
 def perspective(a,r,n,f):
     trig = np.arctan(a/2)
     scale = f/(f-n)
-    return np.matrix([
+    return np.array([
         [trig/r,0,0,0],
         [0,trig,0,0],
         [0,0,scale,1],
         [0,0,-scale*n,0]
     ])
 
-def to3d(vec4):
-    return np.array(vec4).ravel()[0:3]
+def to3d(vec4): return vec4[0:3]
+def to2d(vec4): return vec4[0:2]
 
-def to2d(vec4):
-    return np.array(vec4).ravel()[0:2]
+def unit(v): return v / np.linalg.norm(v)
 
 #------------------------------------------------------------------
 # CLASSES
@@ -88,23 +87,18 @@ class Camera():
         ])
 
     def set_orientation(self, p, v):
-        tmp = np.array([0,1,0])
-        f = v - p
-        self.eye = f
-        f = f / np.linalg.norm(f)
-        r = np.cross(tmp, f)
-        r = r / np.linalg.norm(r)
-        u = np.cross(f, r)
-        u = u / np.linalg.norm(u)
-        #print("r:", r, "len:", np.linalg.norm(r))
-        #print("u:", u, "len:", np.linalg.norm(u))
+        f = unit(v - p)
+        r = unit(np.cross(np.array([0,1,0]), f))
+        u = unit(np.cross(f, r))
 
+        self.eye = f
         self.orientation = np.linalg.inv(np.array([
             [r[0],r[1],r[2],0],
             [u[0],u[1],u[2],0],
             [f[0],f[1],f[2],0],
             [p[0],p[1],p[2],1]
         ]))
+
 
     def toClip(self, model):
         model.transform(self.orientation)
@@ -117,88 +111,73 @@ class Camera():
         model.transform(self.window)
 
     def view(self, model):
-        model.transform(self.orientation @ self.perspective)
-        model.apply(lambda x: x / float(x.T[3]))
+        model.transform(self.orientation)
+        model.transform(self.perspective)
+        model.scale(3)
         model.transform(self.window)
 
 class Model():
     triangles = []
+    normals = []
+
     def __init__(self, diffuse):
         self.m_diff = diffuse
 
     def parse(self, file):
         with open(file, "r") as f:
             for l in f.readlines():
-                tri = Triangle()
-                tri.parse(l)
-                if tri is not None:
-                    self.triangles.append(tri)
+                # String Operations
+                nums = [float(x) for x in l.split() if x not in ['', '\n']]
+                assert len(nums) == 9
+
+                # Convert list into triangles (sets of verts)
+                vertices = [np.array(nums[i:i+3]) for i in range(0, len(nums), 3)]
+                self.triangles.append([np.append(v, [1]) for v in vertices])
+
+                # Compute normals for later operations                
+                v1 = vertices[1] - vertices[0]
+                v2 = vertices[2] - vertices[0]
+                self.normals.append(unit(np.cross(v1,v2)))
     
     def transform(self, mat):
-        for tri in self.triangles: 
-            tri.transform(mat)
+        self.triangles[:] = ([v @ mat for v in verts] for verts in self.triangles)
 
-    def apply(self, fn):
-        for tri in self.triangles:
-            tri.apply(fn)
+    def scale(self, idx):
+        self.triangles[:] = ([v / v[idx] for v in verts] for verts in self.triangles)
+
+    def draw(self, light):
+        ret = []
+        for i in range(len(self.triangles)):
+            # Compute light Vector
+            l_v = unit(light.position - to3d(self.triangles[i][0]))
+            # Compute Color
+            dot = np.dot(self.normals[i], l_v)
+            color = tuple([int(x) for x in (light.color * self.m_diff * max(0, dot))])
+            # Flatten Vectors to 2d
+            pixels = [tuple(to2d(v)) for v in self.triangles[i]]
+            ret.append((pixels, color))
+        return ret
     
-    def append(self, tri):
-        self.triangles.append(tri)
-
-class Triangle():
-    def __init__(self): pass
-
-    def __repr__(self):
-        rep = ""
-        for i in range(len(self.vertices)):
-            rep += "v" + str(i+1) + ": ["
-            for j in range(len(self.vertices[i])):
-                rep += str(self.vertices[i][j])
-                if (j < 3): rep += ", "
-            rep += "]"
-            if (i < 2): rep += ", "
-        return rep
-
-    def parse(self, s):
-        nums = [float(x) for x in s.split() if x not in ['', '\n']]
-        assert len(nums) == 9
-        self.vertices = [np.array(nums[i:i+3] + [1]) for i in range(0, len(nums), 3)]
-
-        v1 = to3d(self.vertices[1]) - to3d(self.vertices[0])
-        v2 = to3d(self.vertices[2]) - to3d(self.vertices[0])
-        self.normal = np.cross(v1, v2)
-
-    def transform(self, mat):
-        self.vertices[:] = (v @ mat for v in self.vertices)
-
-    def apply(self, fn):
-        self.vertices[:] = (fn(v) for v in self.vertices)
-
-    def sub(self, vec3):
-        return to3d(self.vertices[0]) - vec3
-    
-    def draw(self):
-        return [tuple(to2d(v)) for v in self.vertices]
-
-    def light(self, light, diffuse):
-        dot = np.dot(self.normal, self.sub(light.position))
-        return tuple([int(x) for x in (light.color * diffuse * max(0, dot))])
+    def cull(self, eye):
+        self.triangles = list(self.triangles[i] for i in range(len(self.triangles)) if np.dot(self.normals[i], eye) > 0)
 
 class Light():
     def __init__(self, pos, color):
         self.position = pos
         self.color = color
 
-light = Light(np.array([0,0,10]), np.array([255,0,0]))
-
-mod = Model(1/(4*np.pi))
-mod.parse("assets/shark_ag.raw")
-#mod.transform(rotate(0,np.pi,0))
-mod.transform(scale(.5,.5,.5))
-#mod.transform(trans(0,-5,20))
+light = Light(np.array([100,100,0]), np.array([255,255,255]))
 
 cam = Camera(aspect, fov, 1, 100, imgx, imgy)
 cam.set_orientation(np.array([0,0,10]), np.array([0,0,0]))
+
+mod = Model(1)
+mod.parse("assets/mew_lp.raw")
+#mod.transform(rotate(0,np.pi,0))
+#mod.transform(scale(2,2,2))
+#mod.scale(2)
+#mod.transform(trans(0,-5,20))
+#mod.cull(cam.eye)
 cam.view(mod)
 
 '''
@@ -212,8 +191,11 @@ it seems like the the camera orientation v1 is the side, v2 in front, and v3 abo
 image = Image.new("RGB", (imgy, imgx))
 draw = ImageDraw.Draw(image)
 
-for tri in mod.triangles:
+for (pix, color) in mod.draw(light):
+    draw.polygon(pix, fill = color)
+'''
     if (np.dot(tri.normal, tri.sub(cam.eye)) > 0):
         draw.polygon(tri.draw(), fill = tri.light(light,mod.m_diff))
+'''
 
-image.save("test.png", "PNG")
+image.save("test_new.png", "PNG")
